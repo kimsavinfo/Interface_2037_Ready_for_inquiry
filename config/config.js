4668489d9d18cbee0eb65d324bf2dfac98060932
Julien Riffard <jriffarddev@gmail.com>
2015-03-05 14:43:47 +0100

    Utilisation d'une bdd en ligne

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 module.exports = {
 	db : {
-		host : 'mongodb://localhost/interface2037'
+		host : 'mongodb://julien:azerty@ds063879.mongolab.com:63879/interface2037'
+		//host : 'mongodb://localhost/interface2037'
 	},
 
 	environnement : 'Developpement'
-}
+};
