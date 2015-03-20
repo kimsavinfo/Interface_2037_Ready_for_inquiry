2c4f9b0b997989269e6531d2bc628c10707c387e
Kim SAVAROCHE <kimsavinfo@MacBook-Air-de-Kim.local>
2015-03-20 11:51:24 +0100

    OPTIONS client

--- app.js
+++ app.js
@@ -19,22 +19,7 @@ require('./controllers/expert')(app, config.db.host);
 require('./controllers/client')(app, config.db.host);
 
 app.use(function (req, res) {
-	if (req.method === 'OPTIONS') {
-		var headers = {};
-		// IE8 does not allow domains to be specified, just the *
-		// headers["Access-Control-Allow-Origin"] = req.headers.origin;
-		headers["Access-Control-Allow-Origin"] = "*";
-		headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-		headers["Access-Control-Allow-Credentials"] = false;
-		headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-		headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-		res.writeHead(200, headers);
-		res.end();
-	} 
-	else 
-	{
-		res.status(404).send();
-	}
+	res.status(404).send();
 });
 
 module.exports = server;
